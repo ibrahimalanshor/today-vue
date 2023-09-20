ee6e8332db22d576a28feefc5078c60f6adc0689
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-09-20 20:48:05 +0700

    feat: sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+import AppSidebar from 'src/components/partials/app/app-sidebar.vue';
+</script>
+
 <template>
-  <div>
-    test
+  <div class="max-w-2xl mx-auto py-8 grid grid-cols-12">
+    <div class="col-span-4">
+      <app-sidebar />
+    </div>
   </div>
 </template>
---
+++ src/components/partials/app/app-sidebar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { PlainVueList } from 'plain-vue';
+import { ref } from 'vue';
+
+const active = ref<string>('Today');
+</script>
+
+<template>
+  <aside>
+    <plain-vue-list
+      wrapper-class="space-y-1"
+      item-class="py-2 px-3 rounded-md text-sm  cursor-pointer leading-6 font-semibold"
+      inactive-class="text-gray-700 hover:bg-gray-50"
+      active-class="bg-gray-900 text-white hover:bg-gray-900 cursor-default"
+      :items="['Today', 'Late', 'Calendar']"
+      v-model="active"
+    />
+  </aside>
+</template>
