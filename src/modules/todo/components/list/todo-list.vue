9501bcb23d90a0f9e9f7934f0603a1492f77834f
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-09-22 07:25:04 +0700

    feat: visible create input

--- src/modules/todo/components/list/todo-list.vue
+++ src/modules/todo/components/list/todo-list.vue
@@ -54,6 +54,11 @@ const todos = ref<Todo[]>([
     isDone: true,
   },
 ]);
+const visibleCreateInput = ref(false);
+
+function handleCreate() {
+  visibleCreateInput.value = true;
+}
 </script>
 
 <template>
@@ -62,7 +67,7 @@ const todos = ref<Todo[]>([
       <plain-vue-text base-class="text-sm font-semibold text-gray-900">{{
         title
       }}</plain-vue-text>
-      <plain-vue-button v-if="withCreate">
+      <plain-vue-button v-if="withCreate" v-on:click="handleCreate">
         <plus-icon class="w-4 h-4 text-gray-500" />
       </plain-vue-button>
     </div>
@@ -72,7 +77,7 @@ const todos = ref<Todo[]>([
           <todo-list-item v-model="todos[index]" />
         </template>
       </template>
-      <template v-if="withCreate" #append-item>
+      <template v-if="withCreate && visibleCreateInput" #append-item>
         <div>
           <plain-vue-input
             input-class="py-2 px-3 w-full text-sm border-0 rounded-md focus:ring-0 placeholder:text-gray-400 text-gray-700"
