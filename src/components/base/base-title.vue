fedf338ca144b5a9dd7b28ae2b0aa99e51c553f4
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-09-23 18:35:10 +0700

    feat: base

---
+++ src/components/base/base-title.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { PlainVueTitle } from 'plain-vue';
+
+defineProps({
+  level: Number,
+});
+</script>
+
+<template>
+  <plain-vue-title
+    base-class="font-bold leading-6 truncate-tight text-gray-900"
+    :level="level"
+    :level-class="{
+      6: 'text-lg',
+    }"
+    ><slot
+  /></plain-vue-title>
+</template>
